15608855310150620b809fce51647d642d1bb5c0
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2025-10-06 14:17:03 +0300

    frontend work

--- julia_toys/toys/views.py
+++ julia_toys/toys/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import ListView
 
 from toys.models import Toy
@@ -7,3 +9,8 @@ class ToyListView(ListView):
     template_name = 'toys/toys.html'
     model = Toy
     context_object_name = 'toys'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        queryset = queryset.prefetch_related('images')
+        return queryset
